6c9cfd618aacc5ab3aab66875933f4abc312795b
thaines <thaines@6fd0c754-875b-11dd-aace-ddc5e87cd236>
2010-02-15 11:50:52 +0000

    Creation of light_dir files pair.

---
+++ trunk/eos/src/eos/fit/light_dir.h
@@ -0,0 +1,23 @@
+#ifndef EOS_FIT_LIGHT_DIR_H
+#define EOS_FIT_LIGHT_DIR_H
+//------------------------------------------------------------------------------
+// Copyright 2008 Tom Haines
+
+/// \file light_dir.h
+/// Given an image of irradiance values, segments and surface orientation 
+/// distributions this infers the light source direction.
+
+#include "eos/types.h"
+
+namespace eos
+{
+ namespace fit
+ {
+//------------------------------------------------------------------------------
+
+
+
+//------------------------------------------------------------------------------
+ };
+};
+#endif
