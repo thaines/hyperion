c832e053ad2b27f0d238b9fb02d7257153a2f035
thaines <thaines@6fd0c754-875b-11dd-aace-ddc5e87cd236>
2010-02-15 11:49:55 +0000

    Creation of MeanGridSeg and its interface.

--- eos/src/eos/filter/seg_k_mean_grid.h
+++ eos/src/eos/filter/seg_k_mean_grid.h
@@ -8,14 +8,80 @@
 /// segments.
 
 #include "eos/types.h"
+#include "eos/bs/colours.h"
+#include "eos/ds/arrays2d.h"
+#include "eos/time/progress.h"
+#include "eos/svt/field.h"
 
 namespace eos
 {
  namespace filter
  {
 //------------------------------------------------------------------------------
+/// Segmentation algorithm, based on the one used in a past stereopsis paper I 
+/// can no longer find.
+/// Uses k-means, initialised with the image divided into a fine grid - the 
+/// k-means then distorts the grid to the image. This inevitably leads to an 
+/// oversegmentation, but it also limits the size of segments to a relativly
+/// tight range, making it useful for certain kinds of algorithm.
+/// The small segments also limit the potential affect of the inevitable errors.
+class EOS_CLASS MeanGridSeg
+{
+ public:
+  /// &nbsp;
+   MeanGridSeg();
+
+  /// &nbsp;
+   ~MeanGridSeg();
 
 
+  /// Provides the input image, must be called before Run.
+   void SetImage(const svt::Field<bs::ColourLuv> & in);
+   
+  /// Sets the grid size, defaults to 12 pixels (in each dimension.) - this is 
+  /// used to initialise the k means algorithm.
+   void SetSize(nat32 dim);
+   
+  /// Sets the relative costs of the 2 distances involved - colour and spatial distance.
+  /// Defaults are 2 for colMult and 1 for spatialMult.
+   void SetDist(real32 colMult,real32 spatialMult);
+   
+  /// Sets the maximum number of iterations - defaults to 1000.
+   void SetMaxIters(nat32 iters);
+
+
+  /// &nbsp;
+   void Run(time::Progress * prog = null<time::Progress*>());
+
+
+  /// Returns the number of segments found.  
+   nat32 Segments() const;
+
+  /// Extracts the resulting segmentation. Must be the same size as the input image.
+   void GetSegments(svt::Field<nat32> & out) const;
+
+
+  /// &nbsp;
+   inline cstrconst TypeString() const {return "eos::filter::MeanGridSeg";}
+
+
+ private:
+  // Parameters...
+   nat32 dim;
+   real32 colMult;
+   real32 spatialMult;
+   nat32 maxIters;
+   
+  // Input...
+   svt::Field<bs::ColourLuv> image;
+   
+  // Output...
+   nat32 segments;
+   ds::Array2D<nat32> out;
+ 
+  // Runtime...
+   
+};
 
 //------------------------------------------------------------------------------
  };
