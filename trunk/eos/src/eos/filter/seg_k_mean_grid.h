bc2bffe57bd360c3de864f8ccb341087d46e114a
thaines <thaines@6fd0c754-875b-11dd-aace-ddc5e87cd236>
2010-02-15 11:49:42 +0000

    New files for previous commit.

---
+++ trunk/eos/src/eos/filter/seg_k_mean_grid.h
@@ -0,0 +1,23 @@
+#ifndef EOS_FILTER_SEG_K_MEAN_GRID_H
+#define EOS_FILTER_SEG_K_MEAN_GRID_H
+//------------------------------------------------------------------------------
+// Copyright 2008 Tom Haines
+
+/// \file seg_k_mean_grid.h
+/// Provides a segmentation algorithm designed to produce lots of similar sized 
+/// segments.
+
+#include "eos/types.h"
+
+namespace eos
+{
+ namespace filter
+ {
+//------------------------------------------------------------------------------
+
+
+
+//------------------------------------------------------------------------------
+ };
+};
+#endif
