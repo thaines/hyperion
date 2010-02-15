36adfa99f81cdb7b3fbd5d5bc448ab970cb19ca0
thaines <thaines@6fd0c754-875b-11dd-aace-ddc5e87cd236>
2010-02-15 11:36:47 +0000

    Addition of Fisher distribution fitting to disparity maps

---
+++ trunk/eos/src/eos/fit/disp_fish.h
@@ -0,0 +1,26 @@
+#ifndef EOS_FIT_DISP_FISH_H
+#define EOS_FIT_DISP_FISH_H
+//------------------------------------------------------------------------------
+// Copyright 2008 Tom Haines
+
+/// \namespace eos::math
+/// Provides tools for 'fitting' to data sets.
+
+/// \file disp_fish.h
+/// Provides an algorithm for fitting Fisher distributions to disparity maps, 
+/// given the DSI.
+
+#include "eos/types.h"
+
+namespace eos
+{
+ namespace fit
+ {
+//------------------------------------------------------------------------------
+
+
+
+//------------------------------------------------------------------------------
+ };
+};
+#endif
