b45ea3e79f4c100d8b26634f7644c15cb97f93ed
thaines <thaines@6fd0c754-875b-11dd-aace-ddc5e87cd236>
2010-02-15 11:56:04 +0000

    Omi1tted new files of previous commit

---
+++ trunk/ares/src/cyclops/cam_response.h
@@ -0,0 +1,84 @@
+#ifndef CYCLOPS_CAM_RESPONSE_H
+#define CYCLOPS_CAM_RESPONSE_H
+//------------------------------------------------------------------------------
+// Copyright 2008 Tom Haines
+
+#include "cyclops/main.h"
+
+//------------------------------------------------------------------------------
+// Calculates a camera response function given a set of images of a white 
+//0 object where only the exposure time varies.
+class CamResponse
+{
+ public:
+   CamResponse(Cyclops & cyclops);
+  ~CamResponse();
+
+
+ private:
+  Cyclops & cyclops;
+
+  gui::Window * win;
+  gui::Canvas * canvas;
+  
+  gui::ComboBox * viewSelect;
+  gui::ComboBox * algSelect;
+  
+  gui::Expander * brutalFish;
+  gui::EditBox * bfMinAlb;
+  gui::EditBox * bfMaxAlb;
+  gui::EditBox * bfMaxSegCost;
+  gui::EditBox * bfPruneThresh;
+  gui::EditBox * bfIrrErr;
+  gui::EditBox * bfSampleSubdiv;
+  gui::EditBox * bfFurtherSubdiv;
+  gui::EditBox * bfAlbRecursion;
+
+
+  gui::Label * lightDir;
+  gui::Label * lightDir2;
+  bs::Normal lightD;
+  
+  struct Sample
+  {
+   real32 cost;
+   bs::Normal dir;
+  };
+  ds::Array<Sample> samples;
+  ds::Array<real32> albedo;
+
+
+  math::Func crf;
+
+  svt::Var * irrVar;
+  svt::Field<bs::ColourRGB> irr;
+  svt::Var * segVar;
+  svt::Field<nat32> seg;
+  svt::Var * dispVar;
+  svt::Field<math::Fisher> fish;
+
+  svt::Var * imageVar;
+  svt::Field<bs::ColourRGB> image;
+
+  svt::Var * imgVar;
+  svt::Field<bs::ColRGB> img;
+
+
+  void Quit(gui::Base * obj,gui::Event * event);
+
+  void Resize(gui::Base * obj,gui::Event * event);
+
+  void LoadIrr(gui::Base * obj,gui::Event * event);
+  void LoadCRF(gui::Base * obj,gui::Event * event);
+  void LoadSeg(gui::Base * obj,gui::Event * event);
+  void LoadDisp(gui::Base * obj,gui::Event * event);
+  void Run(gui::Base * obj,gui::Event * event);
+  void ChangeView(gui::Base * obj,gui::Event * event);
+  void ChangeAlg(gui::Base * obj,gui::Event * event);
+  void SaveView(gui::Base * obj,gui::Event * event);
+
+  void Update();
+};
+
+//------------------------------------------------------------------------------
+#endif
