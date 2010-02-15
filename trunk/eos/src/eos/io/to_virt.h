4c62b3dd54d5925ce76fb12b9d4dac6b609c0a26
thaines <thaines@6fd0c754-875b-11dd-aace-ddc5e87cd236>
2010-02-15 12:15:28 +0000

    Addition of GPL license to code, ready for release.

--- trunk/eos/src/eos/io/to_virt.h
+++ trunk/eos/src/eos/io/to_virt.h
@@ -3,6 +3,21 @@
 //------------------------------------------------------------------------------
 // Copyright 2005 Tom Haines
 
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+
 /// \file to_virt.h
 /// Provides the techneques to generate virtual io objects that reference
 /// non-virtual io objects, so you can pass non-virtual io objects into methods
